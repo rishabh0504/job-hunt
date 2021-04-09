a0e7beb04a58c8ac997446d16c74844d12f1d78a
rishabh0504 <rishabh.tiwari@synacor.com>
2021-04-09 11:42:06 +0400

    1) Added User details screen. 2) Moved common components to single importable place.

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Select from './select';
+import Input from './input';
+import Checkbox from './checkbox';
+import Button from './button';
+
+export {
+    Select,
+    Button,
+    Checkbox,
+    Input
+}
